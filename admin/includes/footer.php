c43b337f7318084a717d5e2ebf461ee4f9e1707f
CamlianJiezel <bg201803286@wmsu.edu.ph>
2021-04-07 21:01:40 +0845

    First commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>WMSU ICS Voting System</b>
+    </div>
+    <strong> &copy; Created by PACBOG Development Team</strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>WMSU ICS Voting System</b>
+      </div>
+      <strong> &copy; Created by PACBOG Development Team</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
